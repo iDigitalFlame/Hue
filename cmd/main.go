211ab7bff63974abee4c5108a5ef44975468856d
iDigitalFlame <idf@idigitalflame.com>
2022-12-13 15:09:42 -0500

    Fixed some error messages and added a "-list" instruction.

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,9 @@ Usage:
   huectl -k <API KEY> -a <Hub IP/Address> -t <Target Room/Zone>
 
   Optional Arguments:
+    -list
+        List all Groups and Lights that can be targeted. If this is supplied
+        no other optional arguments are parsed.
     -on
         Turn on a Light or Control. Takes precedence over "-off".
     -off
@@ -71,7 +74,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 func main() {
 	var (
 		trans                       time.Duration
-		on, off                     bool
+		on, off, list               bool
 		bright, sat, temp           int
 		target, hex, rgb, addr, key string
 		f                           = flag.NewFlagSet("huectl", flag.ExitOnError)
@@ -81,6 +84,7 @@ func main() {
 	f.StringVar(&target, "t", "", "")
 	f.BoolVar(&on, "on", false, "")
 	f.BoolVar(&off, "off", false, "")
+	f.BoolVar(&list, "list", false, "")
 	f.StringVar(&hex, "hex", "", "")
 	f.StringVar(&rgb, "rgb", "", "")
 	f.IntVar(&sat, "sat", -1, "")
@@ -96,15 +100,16 @@ func main() {
 	}
 
 	if len(key) == 0 {
-		os.Stderr.WriteString("The key value \"-k\" cannot be empty!\n")
+		os.Stderr.WriteString(`The key value "-k" cannot be empty!` + "\n")
 		os.Exit(1)
 	}
 	if len(addr) == 0 {
-		os.Stderr.WriteString("The address value \"-a\" cannot be empty!\n")
+		os.Stderr.WriteString(`The address value "-a" cannot be empty!` + "\n")
 		os.Exit(1)
 	}
-	if len(target) == 0 {
-		os.Stderr.WriteString("The target value \"-t\" cannot be empty!\n")
+
+	if !list && len(target) == 0 {
+		os.Stderr.WriteString(`The target value "-t" cannot be empty!` + "\n")
 		os.Exit(1)
 	}
 
@@ -115,32 +120,72 @@ func main() {
 	if len(rgb) > 0 {
 		v := strings.Split(strings.TrimSpace(rgb), ",")
 		if len(v) != 3 {
-			os.Stderr.WriteString("Invalid RGB value \"" + rgb + "\"!\n")
+			os.Stderr.WriteString(`Invalid RGB value "` + rgb + `"!` + "\n")
 			os.Exit(1)
 		}
 		_ = v[2]
 		if r, err = strconv.ParseUint(strings.TrimSpace(v[0]), 10, 8); err != nil {
-			os.Stderr.WriteString("Invalid RGB \"R\" value \"" + rgb + "\": " + err.Error() + "!\n")
+			os.Stderr.WriteString(`Invalid RGB "R" value "` + rgb + `": ` + err.Error() + "!\n")
 			os.Exit(1)
 		}
 		if g, err = strconv.ParseUint(strings.TrimSpace(v[1]), 10, 8); err != nil {
-			os.Stderr.WriteString("Invalid RGB \"G\" value \"" + rgb + "\": " + err.Error() + "!\n")
+			os.Stderr.WriteString(`Invalid RGB "G" value "` + rgb + `": ` + err.Error() + "!\n")
 			os.Exit(1)
 		}
 		if b, err = strconv.ParseUint(strings.TrimSpace(v[2]), 10, 8); err != nil {
-			os.Stderr.WriteString("Invalid RGB \"B\" value \"" + rgb + "\": " + err.Error() + "!\n")
+			os.Stderr.WriteString(`Invalid RGB "B" value "` + rgb + `": ` + err.Error() + "!\n")
 			os.Exit(1)
 		}
 	}
 
 	x, err := hue.Connect(addr, key)
 	if err != nil {
-		os.Stderr.WriteString("Failed to connect to \"" + addr + "\": " + err.Error() + "!\n")
+		os.Stderr.WriteString(`Failed to connect to "` + addr + `": ` + err.Error() + "!\n")
 		os.Exit(1)
 	}
+
+	if list {
+		g, err := x.Groups()
+		if err != nil {
+			os.Stderr.WriteString(`Could not get the Groups list from "` + addr + `": ` + err.Error() + "!\n")
+			os.Exit(1)
+		}
+		os.Stdout.WriteString("Groups List\n================\n")
+		for k, v := range g {
+			os.Stdout.WriteString(
+				"[" + k + "] " + v.Name() + ": " + v.Type.String() + " Lights " +
+					strconv.FormatUint(uint64(len(v.Lights)), 10) + "\n",
+			)
+		}
+
+		l, err := x.Lights()
+		if err != nil {
+			os.Stderr.WriteString(`Could not get the Lights list from "` + addr + `": ` + err.Error() + "!\n")
+			os.Exit(1)
+		}
+		os.Stdout.WriteString("\nLights List\n================\n")
+		for k, v := range l {
+			if os.Stdout.WriteString("[" + k + "] " + v.Name() + ": " + v.Model + " "); v.IsOn() {
+				os.Stdout.WriteString("On Brightness " + strconv.FormatUint(uint64((float32(v.Brightness())/254.0)*100.0), 10) + "% ")
+				if v.IsColor() {
+					os.Stdout.WriteString(
+						"Hue: " + strconv.FormatUint(uint64(v.Hue()), 10) +
+							" Sat: " + strconv.FormatUint(uint64(v.Saturation()), 10) +
+							" Temp: " + strconv.FormatUint(uint64(v.Temperature()), 10),
+					)
+				}
+				os.Stdout.WriteString("\n")
+				continue
+			}
+			os.Stdout.WriteString("Off\n")
+		}
+
+		os.Exit(0)
+	}
+
 	i := x.GroupByName(target)
 	if i == nil {
-		os.Stderr.WriteString("Could not find target \"" + target + "\" on \"" + addr + "\"!\n")
+		os.Stderr.WriteString(`Could not find target "` + target + `" on "` + addr + `"!` + "\n")
 		os.Exit(1)
 	}
 
@@ -198,7 +243,7 @@ func main() {
 	}
 
 	if err != nil {
-		os.Stderr.WriteString("Error changing lights: " + err.Error() + "!\n")
+		os.Stderr.WriteString("Error changing requested lights: " + err.Error() + "!\n")
 		os.Exit(1)
 	}
 	os.Exit(0)
